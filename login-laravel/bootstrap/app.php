681c99130aff176a53a57580a973065309e87960
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-04-25 11:13:20 +0700

    middlewere auth

--- login-laravel/bootstrap/app.php
+++ login-laravel/bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
             'checkadmin' => \App\Http\Middleware\CheckAdmin::class,
             'checkuser' => \App\Http\Middleware\CheckUser::class,
         ]);
